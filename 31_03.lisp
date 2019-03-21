9183c544e010ee6caaebf685d287d7be05e0f695
tkachenkosergey <43620917+tkachenkosergey@users.noreply.github.com>
2019-03-21 09:25:35 +0300

    Update 31_03.lisp

--- 31_03.lisp
+++ 31_03.lisp
@@ -5,6 +5,17 @@
              (cond ((null lst) nil)
                 (( eq x w) (cons r (rep y w r)))
                 ((listp x) (cons (rep x w r) (rep y w r)))
-                (t (cons x (rep y w r))))   )  
+                (t (cons x (rep y w r)))))  
         
     (car lst)  ( cdr lst ) ) ) 
+
+Test:
+
+;>(print (rep '(1 2 3 1 1) 1 'a))
+;>(print (rep '((1 2 3) 4 5 6 1) 1 44))
+;>(print (rep '( 1 2 3 ((1 2 3) (1 2 3))) 1 '( 4 5 )))
+
+
+;(A 2 3 A A)
+;((44 2 3) 4 5 6 44)
+;((4 5) 2 3 (((4 5) 2 3) ((4 5) 2 3))) 
