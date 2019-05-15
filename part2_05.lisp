0e44ac5903fc0a00fc3a50c2dec63dc9db62075e
tkachenkosergey <43620917+tkachenkosergey@users.noreply.github.com>
2019-05-16 00:48:11 +0300

    Update part2_05.lisp

--- part2_05.lisp
+++ part2_05.lisp
@@ -2,11 +2,11 @@
 тинен, когда, являющейся функциональным аргументом предикат пред исти-
 нен хотя бы для одного элемента списка список.
 
-(defun mypred (func  args)
+(defun pred (func  args)
              (mapcan func args)  
-  )
+   )
 
 
-(print (mypred #' zerop '(1 2 3 4) )) ;=> NIL
-(print (mypred #' atom '(1 2 3 4) )) ;=> T 
-(print (mypred #' zerop '(1 2 4 0) )) ;=> T
+(print (pred #' zerop '(1 2 3 4) )) ;=> NIL
+(print (pred #' atom '(1 2 3 4) )) ;=> T 
+(print (pred #' zerop '(1 2 4 0) )) ;=> T
