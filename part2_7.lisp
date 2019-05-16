df6f92c104c7a2fd74a10683ee9a0e41dc3a8d45
tkachenkosergey <43620917+tkachenkosergey@users.noreply.github.com>
2019-05-16 09:21:31 +0300

    Update part2_7.lisp

--- part2_7.lisp
+++ part2_7.lisp
@@ -3,8 +3,8 @@
 предикат пред
 
 (defun УДАЛТЬ-ЕСЛИ-НЕ (prop lst)
-    (mapcan #'(lambda (x) (if (funcall prop x) (list x)))  lst )
+    (mapcan #'(lambda (x) (if (funcall prop x) (list x))) lst)
 )
 
-(print ( УДАЛТЬ-ЕСЛИ-НЕ #'oddp '(1 2 3 4 5 6 8 9)) ) ;=>(1 3 5 9)
-(print ( УДАЛТЬ-ЕСЛИ-НЕ #'atom '(1 2 3 (4) 5 (6) 8 9)) ) ;=>(1 2 3 5 8 9)
+(print ( УДАЛТЬ-ЕСЛИ-НЕ #'oddp '(1 2 3 4 5 6 8 9))) ;=>(1 3 5 9)
+(print ( УДАЛТЬ-ЕСЛИ-НЕ #'atom '(1 2 3 (4) 5 (6) 8 9))) ;=>(1 2 3 5 8 9)
